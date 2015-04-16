a0bd8adeab83ee799f41c37394365ac268f1bcac
Fred Smalley <fred926@nmsu.edu>
2015-04-16 14:06:49 -0600

    Initial Commit

---
+++ VCFPopStat/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  VCFPopStat
+//
+//  Created by Fred Smalley on 4/16/15.
+//  Copyright (c) 2015 Song Lab. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+    return 0;
+}
